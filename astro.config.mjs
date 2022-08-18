bfb9cac22bb16e9068186e12968d98880c4c3586
Ciarán Ainsworth <ciaranainsworth@gmail.com>
2022-08-18 06:23:30 +0200

    Remove unnecessary plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: netlify(),
+  output: 'static',
 });
