996ee1f6a6eea991648885ecd526d3e06d0da8cb
Ciarán Ainsworth <ciaranainsworth@gmail.com>
2022-08-21 15:32:01 +0200

    Migrate to typography and mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'static',
+  integrations: [tailwind(), mdx(), sitemap()],
+  output: 'static'
 });
